1c4501f99fe4fe5401fc8319c26b34a9b02938c1
Nuyens Gert <nuyensgert@gmail.com>
2012-12-16 00:28:16 +0100

    initial commit

---
+++ src/js/ng-i18n.js
@@ -0,0 +1,9 @@
+/**
+ * Angular I18n - The companion suite for AngularJS
+ * @version v0.1.0 - 2012-12-15
+ * @link http://ng-i18n.github.com
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+angular.module('ngI18nConfig', []).value('ngI18nConfig', {});
+angular.module('ngI18n', ['ngI18nService', 'ngI18nConfig']);
